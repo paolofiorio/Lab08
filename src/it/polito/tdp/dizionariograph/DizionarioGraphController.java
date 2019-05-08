17b7d268ade3cae3c07159a1e18840ab26e6bcaa
Paolo Fiorio <Paolo Fiorio@DESKTOP>
2019-05-08 14:46:19 +0200

    interfaccia

--- src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,91 @@
 package it.polito.tdp.dizionariograph;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DizionarioGraphController {
+
+	private Model model;
 	
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private TextField txtLettere;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private Button btnGeneraGrafo;
+
+    @FXML
+    private Button btnTrovaVicini;
+
+    @FXML
+    private Button btnGradoMax;
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    void doGeneraGrafo(ActionEvent event) {
+
+    }
+
+    
+
+    @FXML
+    void doTrovaGradoMax(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+
+    }
+    
+    @FXML
+    void doReset(ActionEvent event) {
+    	txtLettere.clear();
+    	txtParola.clear();
+    	txtRisultato.clear();	
+    	
+    	txtLettere.setDisable(false);
+    	txtParola.setDisable(true);
+    	btnGeneraGrafo.setDisable(false);
+    	btnTrovaVicini.setDisable(true);
+    	btnGradoMax.setDisable(true);
+    }
+
+    @FXML
+    void initialize() {
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtLettere != null : "fx:id=\"txtLettere\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGeneraGrafo != null : "fx:id=\"btnGeneraGrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnTrovaVicini != null : "fx:id=\"btnTrovaVicini\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnGradoMax != null : "fx:id=\"btnGradoMax\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+        txtParola.setDisable(true);
+        btnTrovaVicini.setDisable(true);
+    	btnGradoMax.setDisable(true);
+    }
+    public void setModel(Model model) {
+    	this.model=model;
+    }
 }
--- src/it/polito/tdp/dizionariograph/db/ConnectDB.java
+++ src/it/polito/tdp/dizionariograph/db/ConnectDB.java
@@ -8,7 +8,7 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class ConnectDB {
 
-	private static final String jdbcURL = "jdbc:mysql://localhost/dizionario";
+	private static final String jdbcURL = "jdbc:mysql://localhost/dizionario?useTimezone=true&serverTimezone=UTC";
 	private static HikariDataSource ds;
 	
 	public static Connection getConnection() {
